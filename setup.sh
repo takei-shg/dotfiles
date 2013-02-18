99a8a8cf931de851744625c715e3d21a02d033ad
takei.shg <takei.shg@gmail.com>
2013-02-18 13:26:50 +0900

    setup.sh does not work on Ubuntu

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
-ln -s $HOME/dotfiles/.bash_profile $HOME/.bash_profile
-ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
-ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-ln -s $HOME/dotfiles/.dir_colors $HOME/.dir_colors
+# ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
+# ln -s $HOME/dotfiles/.bash_profile $HOME/.bash_profile
+# ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+# ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+# ln -s $HOME/dotfiles/.dir_colors $HOME/.dir_colors
 ln -s $HOME/dotfiles/.screenrc $HOME/.screenrc
 
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
